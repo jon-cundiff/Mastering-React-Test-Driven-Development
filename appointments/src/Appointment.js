71eab3a9eb372f3445441d255c64c5faecd8f605
jon-cundiff <jon.cundiff@gmail.com>
2022-04-11 23:27:16 -0500

    creates ol and children length test and implements in AppointmentsDayView component"

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -4,4 +4,12 @@ export const Appointment = ({ customer: { firstName } }) => (
     <div>{firstName}</div>
 );
 
-export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
+export const AppointmentsDayView = ({ appointments }) => (
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map((appointment) => (
+                <div key={appointment.startsAt} />
+            ))}
+        </ol>
+    </div>
+);
