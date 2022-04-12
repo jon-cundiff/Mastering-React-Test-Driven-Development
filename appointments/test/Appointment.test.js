35d54a622b642e2ba4179154d3e8cab57e770d38
jon-cundiff <jon.cundiff@gmail.com>
2022-04-11 23:41:58 -0500

    writes test for displaying proper times in li elements, and adjusts AppointmentsDayView to do this

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -17,6 +17,7 @@ describe("Appointment", () => {
         render(<Appointment customer={customer} />);
         expect(container.textContent).toMatch("Ashley");
     });
+
     it("renders another customer first name", () => {
         customer = { firstName: "Jordan" };
         render(<Appointment customer={customer} />);
@@ -49,5 +50,22 @@ describe("AppointmentDaysView", () => {
         render(<AppointmentsDayView appointments={appointments} />);
         expect(container.querySelector("ol")).not.toBeNull();
         expect(container.querySelector("ol").children).toHaveLength(2);
+    });
+
+    it("renders each appointment into an li", () => {
+        const today = new Date();
+        const appointments = [
+            { startsAt: today.setHours(12, 0) },
+            { startsAt: today.setHours(13, 0) },
+        ];
+
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelectorAll("li")).toHaveLength(2);
+        expect(container.querySelectorAll("li")[0].textContent).toEqual(
+            "12:00"
+        );
+        expect(container.querySelectorAll("li")[1].textContent).toEqual(
+            "13:00"
+        );
     });
 });
