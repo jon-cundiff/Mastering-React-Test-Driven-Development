646401f1111486df586346533b54a17ed05d87f0
jon-cundiff <jon.cundiff@gmail.com>
2022-04-30 17:52:34 -0500

    populates service select options

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-export const AppointmentForm = () => (
+export const AppointmentForm = ({ selectableServices }) => (
     <form id="appointment">
-        <select name="service"></select>
+        <select name="service">
+            <option />
+            {selectableServices.map((service) => (
+                <option key={service}>{service}</option>
+            ))}
+        </select>
     </form>
 );
+
+AppointmentForm.defaultProps = {
+    selectableServices: [
+        "Cut",
+        "Blow-dry",
+        "Cut & color",
+        "Beard trim",
+        "Cut & beard trim",
+        "Extensions",
+    ],
+};
